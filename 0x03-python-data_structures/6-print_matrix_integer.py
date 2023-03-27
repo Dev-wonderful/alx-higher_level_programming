bd6536779402e914fe19cc4dd1905fbb90d5bbd3
Wonderful <adeonederful20@gmail.com>
2023-03-27 12:08:53 +0100

    ternary addition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         return
     for m in matrix:
         for i in m:
-            print("{:d}".format(i), end=" " if i != m[-1] else end="")
+            print("{:d}".format(i), end=" " if i != m[-1] else "")
         print()
