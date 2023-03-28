2f91f62d7551b9816d52d5def96ff8aaa05b8155
Dev-wonderful <adeonederful20@gmail.com>
2023-03-28 16:08:32 +0100

    safe printing a divide function

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,9 @@
 def safe_print_division(a, b):
     result = 0
     try:
-        if isinstance(a, int) and isinstance(b, int):
-            result += a / b
-            return result
-        else:
-            assert()
-    except (ArithmeticError, AssertionError):
+        result += a / b
+        return result
+    except (ArithmeticError, TypeError):
         return None
     finally:
         print("Inside result: {}".format(result if result else None))
