bc0cc6c929563b89d6461c4c941ce03049ed8328
Dev-wonderful <adeonederful20@gmail.com>
2023-03-28 16:12:16 +0100

    safe printing a divide function

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result += a / b
     except (ArithmeticError, TypeError):
-        return None
+        result = None
     finally:
         print("Inside result: {}".format(result if result else None))
     return result
