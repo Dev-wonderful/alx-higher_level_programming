2d43885390e384832ba265e6324fa96b7e451040
Dev-wonderful <adeonederful20@gmail.com>
2023-03-30 10:13:00 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -73,19 +73,14 @@ class Square:
         if self.__size == 0:
             print()
 
-        counter = self.__position[0]
+         for i in range(self.__size):
+             for j in range(self.__size):
+                if self.__position[1] >= 0:
+                    if self.__position[1] > 0:
+                        for i in range(self.__position[0]):
+                            print("_", end="")
+                    else:
+                        for i in range(self.__position[0]):
+                            print(" ", end="")
 
-        while counter > 0:
-            if self.__position[1] >= 0:
-                if self.__position[1] > 0:
-                    for i in range(self.__position[0]):
-                        print("_", end="")
-                else:
-                    for i in range(self.__position[0]):
-                        print(" ", end="")
-
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="" if j != (self.__size - 1) else "\n")
-
-            counter -= 1
+                print("#", end="" if j != (self.__size - 1) else "\n")
