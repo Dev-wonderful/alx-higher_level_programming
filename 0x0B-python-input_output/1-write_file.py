2777bb933948a4ffa9715e0a54857d7dc172f737
Dev-wonderful <adeonederful20@gmail.com>
2023-04-12 23:54:57 +0000

    [Feature]: write to files

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""A module to write into a file"""
+
+
+def write_file(filename="", text=""):
+    """A function to write a text to a file
+        Args:
+            filename (str): the name of the file
+            text (str): The text to be written into the file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
