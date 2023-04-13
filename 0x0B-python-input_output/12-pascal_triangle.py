4c214e35425b75284a02539b6b9dd0bda0c1b4be
Dev-wonderful <adeonederful20@gmail.com>
2023-04-13 13:32:08 +0100

    [Feature]: pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        if len(triangle) == 0:
+            row = [1]
+            triangle.append(row)
+        else:
+            row = []
+            pre = triangle[i-1]
+            for j in range(len(pre)):
+                if j - 1 >= 0:
+                    x = pre[j-1] + pre[j]
+                    row.append(x)
+                else:
+                    row.append(pre[j])
+                if j + 1 < len(pre):
+                    x = pre[j+1] + pre[j]
+                    row.append(x)
+                else:
+                    row.append(pre[j])
+            triangle.append(row)
+    return triangle
