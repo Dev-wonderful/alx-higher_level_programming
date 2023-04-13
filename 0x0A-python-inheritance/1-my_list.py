829ae27ea8ac1ebaa7376b40278c9e22e47d1f66
Dev-wonderful <adeonederful20@gmail.com>
2023-04-13 15:22:41 +0100

    [Feature]: inheriting a list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,16 +5,9 @@
 class MyList(list):
     """Inherits List class"""
 
-    iterable = []
-
-    def __init__(self, *args):
-        """Initialize the derived class"""
-        super().__init__(self, *args)
-	self.iterable = args
-
     def print_sorted(self):
         """Prints the list in a sorted way but doesn't change the list"""
 
-        item = self.copy(self.iterable)
-        sort = self.sort()
-        return item.sort()
+        item = self.copy()
+        item.sort()
+        print(item)
