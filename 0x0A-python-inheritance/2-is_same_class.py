0c655d1340680b1eeef3c6ca40bf3a5a7836b27b
Dev-wonderful <adeonederful20@gmail.com>
2023-04-13 15:29:42 +0100

    [Feature]: Is the same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3
+
+"""A module to check for same class"""
+
+
 def is_same_class(obj, a_class):
-    """checks for inheritance"""
-    return isinstance(obj, a_class)
+    """checks for inheritance
+        Args:
+            obj: an object
+            a_class: a type of class
+        Returns:
+            bool: True if obj is of type a_class, False otherwise
+    """
+    return type(obj) is a_class
