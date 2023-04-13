d01364f12c871b9745f16094df7d99b163a19033
Dev-wonderful <adeonederful20@gmail.com>
2023-04-13 14:36:12 +0100

    [Feature]: pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,18 +12,16 @@ def pascal_triangle(n):
         else:
             row = []
             pre = triangle[i-1]
-            index = 0
             for j in range(len(pre)):
-                if j - 1 >= 0 and j != index:
-                    x = pre[j-1] + pre[j]
-                    row.append(x)
-                    index += 1
-                elif j - 1 == 0:
+                # if j - 1 >= 0 and j != index:
+                #     x = pre[j-1] + pre[j]
+                #     row.append(x)
+                #     index += 1
+                if j == 0:
                     row.append(pre[j])
                 if j + 1 < len(pre):
                     x = pre[j+1] + pre[j]
                     row.append(x)
-                    index += 1
                 elif j + 1 == len(pre):
                     row.append(pre[j])
             triangle.append(row)
