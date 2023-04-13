aa39be5cd794bfbd8f77910a37d5e1e5ad3cd961
Dev-wonderful <adeonederful20@gmail.com>
2023-04-13 16:54:08 +0100

    [Feature]: Base Geometry Class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module to define a base geometry"""
+
+
+class BaseGeometry:
+    """A base geometry class"""
+
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module to define a base geometry"""
+
+
+class BaseGeometry:
+    """A base geometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
