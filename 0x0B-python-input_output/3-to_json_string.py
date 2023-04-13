b0501de541010c60d4dc3d5cb54e6f7b9759249a
Dev-wonderful <adeonederful20@gmail.com>
2023-04-13 00:34:43 +0000

    [Feature]: serializer

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+
+"""A module to serialize a data (convert to JSON)"""
+
+
+def to_json_string(my_obj):
+    """Serilizing an obj
+        Args:
+            my_obj (object): the object to be serialized
+        Returns:
+            str: the serialized object
+    """
+    return json.dumps(my_obj)
