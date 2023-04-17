2ae0c1625c65f50794b9c369c638671b5c9f02d5
Dev-wonderful <adeonederful20@gmail.com>
2023-04-17 19:17:38 +0100

    [Feature]: Base class to manage ID

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,9 +1,14 @@
 import unittest
+from models.base import Base
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestBase(unittest.TestCase):
+    def test_id(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base(20)
+        self.assertEqual(b1.id, b2.id - 1)  # add assertion here
+        self.assertEqual(b3.id, 20)
 
 
 if __name__ == '__main__':
