6957c9f054052cf2d8608ad20b263d72509f70a5
Dev-wonderful <adeonederful20@gmail.com>
2023-05-19 13:07:42 +0100

    de

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,14 @@
+-- All genres not linked to the Dexter show
+USE `hbtn_0d_tvshows`;
+SELECT `name`
+FROM `tv_genres`
+WHERE `name` NOT IN (
+    SELECT tv_genres.name
+    FROM `tv_genres`
+    LEFT JOIN `tv_show_genres`
+    ON tv_show_genres.genre_id = tv_genres.id
+    LEFT JOIN `tv_shows`
+    ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name
---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- not a comedy show, All shows that aren't comedy shows
+USE `hbtn_0d_tvshows`;
+SELECT `title`
+FROM `tv_shows`
+WHERE `title` NOT IN (
+    SELECT tv_shows.title
+    FROM `tv_shows`
+    LEFT JOIN `tv_show_genres`
+    ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN `tv_genres`
+    ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY tv_shows.title
