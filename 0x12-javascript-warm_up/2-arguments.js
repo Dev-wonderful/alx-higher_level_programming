b34c77dcbc07541e59e142ca4414219c5d5326c0
Dev-wonderful <adeonderful20@gmail.com>
2023-06-13 18:55:28 +0100

    correct output for 1 argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const argsNum = argv.length - 2;
-if (argsNum > 0) {
+if (argsNum > 1) {
   console.log('Arguments found');
+} else if (argsNum > 0) {
+  console.log('Argument found');
 } else {
   console.log('No argument');
 }
