fed851110f975ff7d7fd7d0c590314b859ab8798
Dev-wonderful <adeonderful20@gmail.com>
2023-06-13 23:47:13 +0100

    added a compare function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,11 @@
 const { argv } = require('process');
 
 const numbers = argv.slice(2);
-let max = numbers[0];
 let secondMax = 0;
 if (numbers.length > 1) {
-  for (let i = 0; i < numbers.length; i++) {
-    secondMax = max;
-    max = max > numbers[i] ? max : numbers[i];
-  }
+  numbers.sort((a, b) => a - b);
+  // console.log(numbers);
+  secondMax = numbers[numbers.length - 2];
   console.log(secondMax);
 } else {
   console.log(secondMax);
