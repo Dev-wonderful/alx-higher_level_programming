7ffe3b17e2d8530846c50ce11e1337385ec14eec
Dev-wonderful <adeonderful20@gmail.com>
2023-06-13 21:23:23 +0100

    log a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const { argv } = require('process');
+let value = argv[2];
+value = parseInt(value);
+let square;
+if (value) {
+  for (let i = 0; i < value; i++) {
+    square = '';
+    for (let j = 0; j < value; j++) {
+      square += 'x';
+    }
+    console.log(square);
+  }
+} else {
+  console.log('Missing size');
+}
