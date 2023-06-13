9df62395a71f927c426ce4063cc8ac759c6e9c23
Dev-wonderful <adeonderful20@gmail.com>
2023-06-13 23:59:53 +0100

    let me const

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// This code won't pass semistandard, it's just for illustration
+myVar = 333;
---
+++ 0x12-javascript-warm_up/100-main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// This code won't pass semistandard, it's just for illustration
+myVar = 89;
+require('./100-let_me_const');
+console.log(myVar);
