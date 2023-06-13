d6de798662ccdc488902f990e6fda7840a6ee3c0
Dev-wonderful <adeonderful20@gmail.com>
2023-06-13 18:33:55 +0100

    check argument presence

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 const argsNum = argv.length - 2;
-console.log(argsNum);
+if (argsNum > 0) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
