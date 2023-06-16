869fff1bcde0063f58d5e9a5cf63bd22e3cb129f
Dev-wonderful <adeonederful20@gmail.com>
2023-06-16 23:59:46 +0100

    python sql queries

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,14 @@
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-cur = conn.cursor()
-query = "SELECT * FROM states WHERE `name` REGEXP '^N.*'"
-cur.execute(query)
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+if '__name__' == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE `name` REGEXP '^N.*'"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
