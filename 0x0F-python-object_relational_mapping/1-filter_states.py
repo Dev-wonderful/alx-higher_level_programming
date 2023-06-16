3295ab4248ab3b0c31710709b6fbbdde1c954eb5
Dev-wonderful <adeonederful20@gmail.com>
2023-06-16 19:59:50 +0100

    select names that start with N using regexp

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+cur = conn.cursor()
+query = "SELECT * FROM states WHERE `name` REGEXP '^N.*'"
+cur.execute(query)
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
