611a0e95a85e30c2227ec69415aa938887ba2ffe
Dev-wonderful <adeonederful20@gmail.com>
2023-06-18 00:59:15 +0100

    python sql queries

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Module to select rows with name starting with N"""
+
 import MySQLdb
 import sys
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to take filter input and process against SQL injection"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    if sys.argv[4].find(';') == -1:
+        cur = conn.cursor()
+        query = "SELECT * FROM states WHERE `name` = '{}'".format(sys.argv[4])
+        cur.execute(query)
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+    conn.close()
