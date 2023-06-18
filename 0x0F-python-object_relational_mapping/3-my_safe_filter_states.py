db060d297341708269a07d97eaaa128d6259e09f
Dev-wonderful <adeonederful20@gmail.com>
2023-06-18 02:06:49 +0100

    python sql queries 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,9 +7,9 @@ import sys
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    if sys.argv[4].find(';') == -1:
+    if sys.argv[4].find(';') != -1:
         cur = conn.cursor()
-        query = "SELECT * FROM states WHERE `name` = '{d}'".format(sys.argv[4])
+        query = "SELECT * FROM states WHERE `name` = '{}'".format(sys.argv[4])
         cur.execute(query)
         query_rows = cur.fetchall()
         for row in query_rows:
