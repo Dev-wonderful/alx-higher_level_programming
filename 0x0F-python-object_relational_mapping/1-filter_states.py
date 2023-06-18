347456236aaeaabdbb95e80e8861d1b14b981fd9
Dev-wonderful <adeonederful20@gmail.com>
2023-06-18 02:57:10 +0100

    python sql queries 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE `name` REGEXP '^N.*' ORDER BY id ASC"
-    # cur.execute(query)
-    # query_rows = cur.fetchall()
-    # for row in query_rows:
-    #     print(row)
+    query = "SELECT * FROM states WHERE `name` LIKE BINARY 'N%' ORDER BY id ASC"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
     conn.close()
