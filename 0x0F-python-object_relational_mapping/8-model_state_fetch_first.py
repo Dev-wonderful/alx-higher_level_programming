977c0a2788dba52014392700269a91cee5113d09
Dev-wonderful <adeonederful20@gmail.com>
2023-06-20 20:35:45 +0100

    fetch first state using orm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).limit(1)
+    state = session.query(State).order_by(State.id).first()
 
     if state is not None:
         print("{}: {}".format(state.id, state.name))
