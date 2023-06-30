dfdd636d885f9f57586c5b4c94106c8b48cbb0e7
Dev-wonderful <adeonederful20@gmail.com>
2023-06-30 21:26:09 +0000

    accurate2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # read json file then post content
-curl -s --request POST "$1" -T "$2" -H "Content-Type: application/json"
+curl -s --request POST "$1" -d "@$2" -H "Content-Type: application/json"
