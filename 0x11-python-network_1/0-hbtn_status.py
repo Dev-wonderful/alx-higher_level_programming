ea88188dc0dcd0b0ee3b2315f7e8ab32fc89410f
Dev-wonderful <adeonderful20@gmail.com>
2023-07-01 22:01:18 +0100

    urllib request if main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""request using urllib"""
 import urllib.request as request
 
-req = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(req) as response:
-    value = response.read()
-    print(f"""Body response:
-        - type: {type(value)}
-        - content: {value}
-        - utf8 content: {value.decode()}""")
+if __name__ == '__main__':
+    req = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        value = response.read()
+        print(f"""Body response:
+            - type: {type(value)}
+            - content: {value}
+            - utf8 content: {value.decode()}""")
