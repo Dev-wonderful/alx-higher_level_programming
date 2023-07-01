6034d566dab6c1fd276a2a2e20c1eece2a492c20
Dev-wonderful <adeonderful20@gmail.com>
2023-07-02 00:04:40 +0100

    urllib post request with email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,10 +2,12 @@
 """post request"""
 import sys
 import urllib.request as request
+import urllib.parse as parse
 
 if __name__ == '__main__':
     req = request.Request(sys.argv[1])
-    email = sys.argv[2]
-    email = email.encode()
-    with request.urlopen(req, email) as response:
+    email = {'email': sys.argv[2]}
+    data = parse.urlencode(email)
+    data = data.encode()
+    with request.urlopen(req, data) as response:
         print('{}'.format(response.read().decode()))
