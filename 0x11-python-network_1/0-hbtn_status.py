e057d9569f16f14a89311db6ab8b5f520f1e22ec
Dev-wonderful <adeonderful20@gmail.com>
2023-07-01 22:22:31 +0100

    urllib request if main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,11 @@ if __name__ == '__main__':
     req = request.Request('https://alx-intranet.hbtn.io/status')
     with request.urlopen(req) as response:
         value = response.read()
-        print(f"""Body response:
-            - type: {type(value)}
-            - content: {value}
-            - utf8 content: {value.decode()}""")
+        print('Body response:')
+        print('\t- type: '.format(type(value)))
+        print('\t- content: '.format(value))
+        print('\t- utf8 content: '.format(value.decode()))
+        # print(f"""Body response:
+        #     - type: {type(value)}
+        #     - content: {value}
+        #     - utf8 content: {value.decode()}""")
