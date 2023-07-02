a3185f23abe7961792b17264bf0154307f495bc6
Dev-wonderful <adeonderful20@gmail.com>
2023-07-02 01:25:43 +0100

    get request header using requests module

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""request status using requests module"""
+"""request header using requests module"""
 import sys
 import requests
 
 if __name__ == '__main__':
-    req = requests.get(sys.argv[1])
-    print('{}'.format(req.headers['X-Request-Id']))
+    try:
+        req = requests.get(sys.argv[1])
+        print('{}'.format(req.headers['X-Request-Id']))
+    except:
+        pass
