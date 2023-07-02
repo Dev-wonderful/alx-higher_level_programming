ffaed313e876687531f9d77bd4a99c2f5fa24978
Dev-wonderful <adeonderful20@gmail.com>
2023-07-02 01:39:32 +0100

    get request header using requests module

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,6 +6,7 @@ import requests
 if __name__ == '__main__':
     try:
         req = requests.get(sys.argv[1])
+        # print('{}'.format(req.headers.get('X-Request-Id')))
         print('{}'.format(req.headers['X-Request-Id']))
-    except Exception:
+    except KeyError:
         pass
