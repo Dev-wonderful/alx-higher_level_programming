de8d0f7ddc0931a5b25cdd6ccc124dd94f021a3a
Dev-wonderful <adeonderful20@gmail.com>
2023-07-25 20:11:42 +0100

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = require('process');
+
+function search (todos) {
+  const completedTasks = {};
+  todos.forEach((todo) => {
+    if (todo.completed === true) {
+      if (!(todo.userId in completedTasks)) {
+        completedTasks[todo.userId] = 0;
+      }
+      completedTasks[todo.userId] += 1;
+    }
+  });
+  return completedTasks;
+}
+
+const url = argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    console.log(search(todos));
+  }
+});
