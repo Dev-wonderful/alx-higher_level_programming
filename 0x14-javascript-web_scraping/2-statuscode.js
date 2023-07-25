a24550067430848f9a3f2926cc7108b7648dbbca
Dev-wonderful <adeonderful20@gmail.com>
2023-07-25 16:43:36 +0100

    testing output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,6 @@ const url = argv[2];
 request
   .get(url)
   .on('response', function (response) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode)
+    // console.log(`code: ${response.statusCode}`);
   });
