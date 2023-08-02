d75e8170a020d6bc8406f2c72ec4debe7fe6935b
Dev-wonderful <adeonederful20@gmail.com>
2023-08-02 18:51:03 +0100

    cors error

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
 $.get(url, function (films) {
-  for (const film of films.results) {
-    $('ul#list_movies').append($('<li></li>').text(film.title));
+  for (const film of films?.results) {
+    $('ul#list_movies').append($('<li></li>').text(film?.title));
   }
 });
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,6 @@
 $('document').ready(function () {
   const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
   $.get(url, function (greeting) {
-    $('div#hello').text(greeting.results[0].title);
+    $('div#hello').text(greeting?.hello);
   });
 });
