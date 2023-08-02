7830fb5ed4bf41d23b05acd5cc715bffeb5c6e4d
Dev-wonderful <adeonederful20@gmail.com>
2023-08-02 20:10:08 +0100

    xlick event

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$('document').ready(function () {
+  $('div#add_item').on('click', function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+  $('div#remove_item').on('click', function () {
+    const allItems = $('ul.my_list').children()
+    const length = allItems.length
+    allItems[length - 1].remove()
+  });
+  $('div#clear_list').on('click', function () {
+    $('ul.my_list').html('');
+  })
+});
